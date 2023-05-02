80e3f7b7e31ebc19fabbcf804f57d84b42462f94
Huiping Miao <Huiping.Miao@sas.com>
2023-05-02 14:24:22 -0400

    module 1

--- budget/FrequentExpenses.py
+++ budget/FrequentExpenses.py
@@ -11,14 +11,11 @@ for expense in expenses.list:
 spending_counter = collections.Counter(spending_categories)
 print(spending_counter)
 top5 = spending_counter.most_common(5)
-print("Number of categories = " + str(spending_counter.__len__())) #len(spendingCounter)))
-#print(top5)
+print("Number of categories = " + str(spending_counter.__len__())) 
 
-# zip puts 2 lists into a dict, *zip does the reverse
 categories, count = zip(*top5)
 
-# Graph each spending category count as a bar chart
 fig,ax=plt.subplots()
-ax.bar(categories, count) #, color=[numpy.random.rand(3,) for _ in range(5)])
+ax.bar(categories, count) 
 ax.set_title('# of Purchases by Category')
 plt.show()
